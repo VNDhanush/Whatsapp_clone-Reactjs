805498c3498d346a275d7ad2ac1a9172eb8091fc
Dhanush <vndhanush51@gmail.com>
2020-08-20 12:19:45 +0530

    Added the random profile picture to the sidebar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCMueTEX0dYlg7GNYBJzi2rODI30YWmvLc",
+    authDomain: "whatsapp-clone-2290a.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-2290a.firebaseio.com",
+    projectId: "whatsapp-clone-2290a",
+    storageBucket: "whatsapp-clone-2290a.appspot.com",
+    messagingSenderId: "409268775690",
+    appId: "1:409268775690:web:2190fed275df69c932a8f3",
+    measurementId: "G-61HCF36WYH"
+  };
