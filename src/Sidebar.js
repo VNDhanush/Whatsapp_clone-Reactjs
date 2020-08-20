efc88123ffde6609253131ac7475b3a9ebcf9378
Dhanush <vndhanush51@gmail.com>
2020-08-20 15:51:59 +0530

    Room name and firebase connected

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,27 @@
-import React from "react";
-import "./Sidebar.css";
+import React, { useEffect, useState } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined } from "@material-ui/icons";
+import "./Sidebar.css";
 import SidebarChat from "./SidebarChat";
+import db from "./firebase";
 
 
 function Sidebar() {
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        db.collection("rooms").onSnapshot((snapshot) =>
+            setRooms(snapshot.docs.map((doc) => ({
+                id: doc.id,
+                data: doc.data(),
+            }))
+            )
+        );
+    }, []);
+
     return (
         <div className="sidebar">
             <div className="sidebar__header">
@@ -33,10 +46,11 @@ function Sidebar() {
                 </div>
             </div>
             <div className="sidebar__chats">
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
+                <SidebarChat addNewChat />
+                {rooms.map(room => (
+                    <SidebarChat key={room.id} id={room.id}
+                        name={room.data.name} />
+                ))}
 
             </div>
         </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,18 @@
+import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyCMueTEX0dYlg7GNYBJzi2rODI30YWmvLc",
-    authDomain: "whatsapp-clone-2290a.firebaseapp.com",
-    databaseURL: "https://whatsapp-clone-2290a.firebaseio.com",
-    projectId: "whatsapp-clone-2290a",
-    storageBucket: "whatsapp-clone-2290a.appspot.com",
-    messagingSenderId: "409268775690",
-    appId: "1:409268775690:web:2190fed275df69c932a8f3",
-    measurementId: "G-61HCF36WYH"
-  };
+  apiKey: "AIzaSyCMueTEX0dYlg7GNYBJzi2rODI30YWmvLc",
+  authDomain: "whatsapp-clone-2290a.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-2290a.firebaseio.com",
+  projectId: "whatsapp-clone-2290a",
+  storageBucket: "whatsapp-clone-2290a.appspot.com",
+  messagingSenderId: "409268775690",
+  appId: "1:409268775690:web:2190fed275df69c932a8f3",
+  measurementId: "G-61HCF36WYH"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+export { auth, provider };
+export default db;
